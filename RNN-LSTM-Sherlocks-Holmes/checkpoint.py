79be4246e4d3bd63d3dd0a000f9837a957589487
Canh Tran <trandcanh@gmail.com>
2017-06-14 23:41:23 +0800

    add ceckpoint

--- RNN-LSTM-Sherlocks-Holmes/checkpoint.py
+++ RNN-LSTM-Sherlocks-Holmes/checkpoint.py
@@ -36,3 +36,16 @@ X = np.reshape(dataX, (n_patterns, seq_length, 1))
 X = X / float(n_vocab)
 y = np_utils.to_categorical(dataY)
 # print("Y to categorical: ", y)
+
+# Define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# checkpoint
+filepath = "weights-impovement-{epoch:2d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
