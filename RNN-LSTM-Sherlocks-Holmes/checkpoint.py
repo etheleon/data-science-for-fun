ba783602af5ee71039ec6f41e6b138830d8b9ece
canhtran <trandcanh@gmail.com>
2017-06-15 16:10:58 +0800

    add weight dir

--- RNN-LSTM-Sherlocks-Holmes/checkpoint.py
+++ RNN-LSTM-Sherlocks-Holmes/checkpoint.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
@@ -45,7 +46,7 @@ model.add(Dense(y.shape[1], activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 # checkpoint
-filepath = "weights-impovement-{epoch:2d}-{loss:.4f}.hdf5"
+filepath = "weights/weights-impovement-{epoch:2d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
